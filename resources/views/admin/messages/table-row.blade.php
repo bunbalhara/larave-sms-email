eeb94e6d3b4b06ceed43e4e1f9c10107c9ef7803
bunbalhara <nanjingzhe425@gmail.com>
2020-11-10 00:29:00 +0800

    commit

--- resources/views/admin/messages/table-row.blade.php
+++ resources/views/admin/messages/table-row.blade.php
@@ -6,11 +6,9 @@
         <div>({{$message[0]->sender_name??'-'}})</div>
     </td>
     <td>
-        <div class="d-flex align-items-center position-relative" style="width: 100px; height: 50px;">
         @foreach($message as $i => $item)
             <div><img src="{{asset('assets/img/flags/'.$item->recipient->country.'.png')}}" class="mr-1"> {{$item->recipient->phone_number}}</div>
         @endforeach
-        </div>
     </td>
     <td>{{$message[0]->content??''}}</td>
     <td>{{date('Y-m-d h:m:s', strtotime($message[0]->delivered??time()))}}</td>
