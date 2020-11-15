e9eb03265eb364fefa2a184e5621241c20464d47
bunbalhara <nanjingzhe425@gmail.com>
2020-11-16 06:25:58 +0800

    commit

--- app/Providers/ConfigServiceProvider.php
+++ app/Providers/ConfigServiceProvider.php
@@ -22,23 +22,23 @@ class ConfigServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        config([
-            'mail.default' => option('mail_mailer','smtp'),
-            'mail.mailers.smtp' => [
-                'transport' => 'smtp',
-                'host' => option('mail_host', 'smtp.mailgun.org'),
-                'port' => option('mail_port', 587),
-                'encryption' => option('mail_encryption', 'tls'),
-                'username' => option('mail_username'),
-                'password' => option('mail_password'),
-                'timeout' => null,
-                'auth_mode' => null,
-            ],
-            'mail.mailers.ses' =>[
-                'key' => option('ses_key', ''),
-                'secret' => option('ses_secret', ''),
-                'region' => option('ses_region', ''),
-            ]
-        ]);
+//        config([
+//            'mail.default' => option('mail_mailer','smtp'),
+//            'mail.mailers.smtp' => [
+//                'transport' => 'smtp',
+//                'host' => option('mail_host', 'smtp.mailgun.org'),
+//                'port' => option('mail_port', 587),
+//                'encryption' => option('mail_encryption', 'tls'),
+//                'username' => option('mail_username'),
+//                'password' => option('mail_password'),
+//                'timeout' => null,
+//                'auth_mode' => null,
+//            ],
+//            'mail.mailers.ses' =>[
+//                'key' => option('ses_key', ''),
+//                'secret' => option('ses_secret', ''),
+//                'region' => option('ses_region', ''),
+//            ]
+//        ]);
     }
 }
