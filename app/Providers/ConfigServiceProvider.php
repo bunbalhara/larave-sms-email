2adf05e91b07536f421958a09d22aff940372aac
kyledevelop <61576083+kyledevelop@users.noreply.github.com>
2020-12-11 23:22:33 -0500

    d
    d

--- app/Providers/ConfigServiceProvider.php
+++ app/Providers/ConfigServiceProvider.php
@@ -21,31 +21,31 @@ class ConfigServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        config([
-            'mail.from.address'=>option('mail_from', env('MAIL_FROM_ADDRESS', 'hello@example.com')),
-            'mail.default' => option('mail_mailer','smtp'),
-            'mail.driver'=> option('mail_mailer','smtp'),
-            'mail.mailers.smtp' => [
-                'transport' => 'smtp',
-                'host' => option('mail_host', 'smtp.mailgun.org'),
-                'port' => option('mail_port', 587),
-                'encryption' => option('mail_encryption', 'tls'),
-                'username' => option('mail_username'),
-                'password' => option('mail_password'),
-                'timeout' => null,
-                'auth_mode' => null,
-            ],
-            'mail.mailers.ses' =>[
-                'key' => option('ses_key', ''),
-                'secret' => option('ses_secret', ''),
-                'region' => option('ses_region', ''),
-            ],
-            'services.ses'=>[
-                'key' => option('ses_key', ''),
-                'secret' => option('ses_secret', ''),
-                'region' => option('ses_region', ''),
-            ]
-        ]);
+//     {
+//         config([
+//             'mail.from.address'=>option('mail_from', env('MAIL_FROM_ADDRESS', 'hello@example.com')),
+//             'mail.default' => option('mail_mailer','smtp'),
+//             'mail.driver'=> option('mail_mailer','smtp'),
+//             'mail.mailers.smtp' => [
+//                 'transport' => 'smtp',
+//                 'host' => option('mail_host', 'smtp.mailgun.org'),
+//                 'port' => option('mail_port', 587),
+//                 'encryption' => option('mail_encryption', 'tls'),
+//                 'username' => option('mail_username'),
+//                 'password' => option('mail_password'),
+//                 'timeout' => null,
+//                 'auth_mode' => null,
+//             ],
+//             'mail.mailers.ses' =>[
+//                 'key' => option('ses_key', ''),
+//                 'secret' => option('ses_secret', ''),
+//                 'region' => option('ses_region', ''),
+//             ],
+//             'services.ses'=>[
+//                 'key' => option('ses_key', ''),
+//                 'secret' => option('ses_secret', ''),
+//                 'region' => option('ses_region', ''),
+//             ]
+//         ]);
     }
 }
